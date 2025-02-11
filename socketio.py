c20c386ac659da20ac46bb34dcca56c7b3085add
mechaadi <as808780@gmail.com>
2025-02-12 01:36:54 +0530

    fix: socket io host

--- socketio.py
+++ socketio.py
@@ -63,4 +63,4 @@ def disconnect(sid):
     print('disconnect ', sid)
 
 if __name__=="__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=5002, lifespan="on", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5002, lifespan="on", reload=True)
